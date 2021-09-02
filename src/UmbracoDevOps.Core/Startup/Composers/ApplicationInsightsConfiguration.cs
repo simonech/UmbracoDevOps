90462245a5f1d311db70acd3e6c6ef85a6c32568
Simone Chiaretta <simone@piyosailing.com>
2021-09-02 21:34:45 +0300

    Installed AppInsights

---
+++ src/UmbracoDevOps.Core/Startup/Composers/ApplicationInsightsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace UmbracoDevOps.Core.Startup.Composers
+{
+    public class ApplicationInsightsConfiguration : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            var AiEnabled = ConfigurationManager.AppSettings["My.Azure.AI.Enabled"].InvariantEquals("true");
+            var AiInstrumentationConnectionString = ConfigurationManager.AppSettings["My.Azure.AI.InstrumentationConnectionString"];
+            if (!AiEnabled)
+            {
+                Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.DisableTelemetry = true;
+                return;
+            }
+
+            Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.Active.ConnectionString = AiInstrumentationConnectionString;
+        }
+    }
+}
