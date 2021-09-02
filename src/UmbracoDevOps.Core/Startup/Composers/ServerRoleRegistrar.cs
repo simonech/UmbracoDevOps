a8d1f9cf8fcfab42b5e47f0431e7d7bc2808722e
Simone Chiaretta <simone@piyosailing.com>
2021-09-02 21:01:25 +0300

    Added explicit selection of master scheduler

---
+++ src/UmbracoDevOps.Core/Startup/Composers/ServerRoleRegistrar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Sync;
+
+namespace UmbracoDevOps.Core.Startup.Composers
+{
+    public class ServerRoleRegistrar : IServerRegistrar
+    {
+        public IEnumerable<IServerAddress> Registrations => Enumerable.Empty<IServerAddress>();
+
+        public ServerRole GetCurrentServerRole()
+        {
+            switch (ConfigurationManager.AppSettings["My.Core.LoadBalancing.ServerRole"].ToLowerInvariant())
+            {
+                case "master":
+                    return ServerRole.Master;
+                case "replica":
+                    return ServerRole.Replica;
+                default:
+                    return ServerRole.Single;
+            }
+        }
+
+        public string GetCurrentServerUmbracoApplicationUrl()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/UmbracoDevOps.Core/Startup/Composers/ServerRoleRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace UmbracoDevOps.Core.Startup.Composers
+{
+    public class ServerRoleRegistration : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.SetServerRegistrar(new ServerRoleRegistrar());
+        }
+    }
+}
